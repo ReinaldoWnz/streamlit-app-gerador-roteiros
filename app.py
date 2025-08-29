1fa87ad14298e2b6ef7acab61c94c7f70ed4571a
ReinaldoWnz <122365569+ReinaldoWnz@users.noreply.github.com>
2025-08-29 09:10:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def mostrar_com_streaming(texto):
     for char in texto:
         exibido += char
         placeholder.markdown(exibido)
-        time.sleep(0.01)  # velocidade da "digitação"
+        time.sleep(0.03)  # velocidade da "digitação"
     return exibido
 
 # Tipo do vídeo
